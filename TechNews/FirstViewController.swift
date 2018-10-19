9676009fa231ec6bef840f7194f0c136bbc50b05
杨昊坤 <546297129@qq.com>
2018-10-19 22:03:15 +0900

    Initial Commit

---
+++ TechNews/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TechNews
+//
+//  Created by Felix on 2018/10/19.
+//  Copyright © 2018 Felix. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ TechNews/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TechNews
+//
+//  Created by Felix on 2018/10/19.
+//  Copyright © 2018 Felix. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
